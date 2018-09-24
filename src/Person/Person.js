4edb6b9d04e1ceabeda3d363d88ee79eaacc2ca1
Alex Andrade <g.alex.andrade@gmail.com>
2018-09-24 13:56:37 -0300

    ErrorBoundary

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,6 +3,12 @@ import classes from './Person.css';
 
 const person = (props) => {
 
+    const rnd = Math.random();
+
+    if(rnd > 0.7){
+        throw new Error('Something went wrong');
+    }
+
     return (
         <div className={classes.Person}>
             <p onClick={props.click}>I am {props.name} and I am {props.age} years old!</p>
