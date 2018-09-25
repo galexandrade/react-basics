7d1c0762477ac73febd9c01aa91d01703a0db7ac
Alex Andrade <g.alex.andrade@gmail.com>
2018-09-25 17:44:29 -0300

    HOC

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import classes from './Person.css';
+import Aux from '../../../hoc/Auxiliary';
+import WithClass2 from '../../../hoc/WithClass2';
 
 class Person extends Component{
     constructor(props){
@@ -19,13 +21,13 @@ class Person extends Component{
         console.log('[Person.js] Inside render');
         
         return (
-            <div className={classes.Person}>
+            <Aux>
                 <p onClick={this.props.click}>I am {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name}></input>
-            </div>
+            </Aux>
         );
     }
 }
 
-export default Person;
+export default WithClass2(Person, classes.Person);
---
+++ src/hoc/WithClass2.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+/*
+//Higher Order Components with function operator
+const withClass2 = (WrappedComponent, className) => {
+    return (props) => (
+        <div className={className}>
+            <WrappedComponent {...props}/>
+        </div>
+    );
+}
+*/
+
+//Higher Order Components with class
+const withClass2 = (WrappedComponent, className) => {
+    return class extends Component{
+        render(){
+            return <div className={className}>
+                <WrappedComponent {...this.props}/>
+            </div>
+        }
+    }
+}
+
+export default withClass2;
